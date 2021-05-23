36a1150e0f2b0b61a0f5592e748e92915a89c65a
Pericles S. da C. Jr <pericles.scj@gmail.com>
2021-05-23 20:21:28 -0300

    Add fw tag rule

--- instance.tf
+++ instance.tf
@@ -2,6 +2,8 @@ resource "google_compute_instance" "default" {
     name         = "test"
     machine_type = "e2-medium"
 
+    tags = ["allow-http"]
+
     metadata = {
       ssh-keys = "pjota:${file("~/.ssh/gcp_terraform.pub")}"
     }
