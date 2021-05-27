431e46c3d85298f9a4b31cb54819afe304322cce
Pericles S. da C. Jr <pericles.scj@gmail.com>
2021-05-27 19:44:28 -0300

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region name"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone name"
+}
+
+variable "gcp_vm_count" {
+  description = "Number of VMs"
+}
+
+variable "gcp_vm_name" {
+  description = "GCP Vm name"
+}
+
+variable "gcp_vm_type" {
+  description = "GCP Vm type"
+}
+
+variable "gcp_vm_image" {
+  description = "GCP Vm image"
+}
+
+variable "gcp_vm_network" {
+  description = "GCP Vm network"
+}
