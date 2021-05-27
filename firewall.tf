d4b3e8c030a4f44c4289453dde1397b564a1e45c
Pericles S. da C. Jr <pericles.scj@gmail.com>
2021-05-27 19:41:34 -0300

    Remove fw rules

--- firewall.tf
+++ firewall.tf
@@ -10,3 +10,16 @@ resource "google_compute_firewall" "allow-http" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["allow-http"]
 }
+
+resource "google_compute_firewall" "allow-https" {
+  name    = "allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-https"]
+}
